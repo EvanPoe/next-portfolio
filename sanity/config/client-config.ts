8fd99772d53956a4abbe54429282966354ea89cd
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-07-28 13:23:34 -0500

    adding dynamic routing for individual project pages
    Also organizing client config into its own file and folder

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "b2jwt6no",
+  dataset: "production",
+  apiVersion: "2023-07-25",
+};
+
+export default config;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,14 +1,9 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
+import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: "b2jwt6no",
-    dataset: "production",
-    apiVersion: "2023-07-25",
-  });
-
-  return client.fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type == "project"]{
             _id,
             _createdAt,
@@ -19,5 +14,21 @@ export async function getProjects(): Promise<Project[]> {
             url,
             content
         }`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            alt,
+            url,
+            content
+        }`,
+    { slug }
   );
 }
